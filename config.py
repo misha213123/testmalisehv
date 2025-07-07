f42a57a2980354c2ea9c223d4ca320fa62a0763a
Ivan Ivanov <ivan.ivanov@gmail.com>
2025-07-07 10:16:58 +0200

    Обновил config.py и requirements.txt

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "7915870466:AAEbLG45o1bC5fazzCaMb_0wSbXhFcOdwp4"  # Вставь сюда токен бота
-ADMIN_ID = 551270027  # ID менеджера для уведомлений
+BOT_TOKEN = "7915870466:AAEbLG45o1bC5fazzCaMb_0wSbXhFcOdwp4"
+ADMIN_ID = 551270027
